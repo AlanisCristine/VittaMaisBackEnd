00128727c63c630710bed9ec0f64c1f3ba3a7e70
Alanis Cristine <alanisalmeidads@gmail.com>
2025-05-28 16:48:53 -0300

    Alterações criação do médico

--- VittaMais.API/Models/DTOs/MedicoDTO.cs
+++ VittaMais.API/Models/DTOs/MedicoDTO.cs
@@ -5,7 +5,7 @@ namespace VittaMais.API.Models.DTOs
     public class MedicoDTO
     {
         [Required(ErrorMessage = "O nome é obrigatório.")]
-        [StringLength(100, MinimumLength =2, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O email é obrigatório.")]
@@ -18,5 +18,16 @@ namespace VittaMais.API.Models.DTOs
 
         [Required(ErrorMessage = "A especialidade é obrigatória.")]
         public string EspecialidadeId { get; set; }
+
+        // Campos opcionais (não obrigatório, sem [Required])
+        [StringLength(14, ErrorMessage = "CPF deve ter até 14 caracteres.")]
+        public string? Cpf { get; set; }
+
+        public DateTime? DataNascimento { get; set; }
+
+        [StringLength(20, ErrorMessage = "Telefone deve ter até 20 caracteres.")]
+        public string? Telefone { get; set; }
+
+        public Endereco Endereco { get; set; }
     }
 }
